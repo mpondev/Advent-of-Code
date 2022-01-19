3bc7edc5c04beb1b2afe4e2ae9b73a18963c9d14
mponpol <mponpol@gmail.com>
2022-01-19 21:07:25 +0100

    Day 1 finished

--- 2021/Day 1/script.js
+++ 2021/Day 1/script.js
@@ -2008,16 +2008,30 @@ const entries = `199
 2338
 2341`;
 
-// Convert the string into an array with the split() method, creating an array of substrings. As the entries are separated by a newline character, use its escape sequence to split the string.
-let entriesArr = entries.split("\n");
+// Convert the string into an array with the split() method, creating an array of substrings. As the entries are separated by a newline character, use its escape sequence to split the string. Then convert strings to integers
+let entriesArr = entries.split("\n").map(entry => parseInt(entry));
 
-// Convert the strings to integers
-entriesArr = entriesArr.map((entry) => parseInt(entry));
-
-console.log(entriesArr);
-
-let counter;
-
-for (let i = 1; i < entriesArr.length -1; i++) {
-    (entriesArr[i] > entriesArr[i - 1]) ? counter +=
+// Iterate over array
+let counter = 0;
+for (let i = 1; i < entriesArr.length; i++) {
+    if (entriesArr[i] > entriesArr[i - 1]) {
+        counter += 1;
     }
+}
+
+console.log(counter);
+
+/*
+ * DAY 1 (II)
+ * Consider sums of a three-measurement sliding window. How many sums are larger
+ * than the previous sum?
+ */
+
+counter = 0;
+for (let i = 3; i < entriesArr.length; i++) {
+    let sum1 = entriesArr[i] + entriesArr[i - 1] + entriesArr[i - 2];
+    let sum2 = (entriesArr[i - 1] + entriesArr[i - 2] + entriesArr[i - 3]);
+    if (sum1 > sum2) counter += 1;
+}
+
+console.log(counter);
