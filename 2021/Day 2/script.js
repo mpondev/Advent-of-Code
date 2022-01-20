ac8c2be6992056d04003a994fe7fa9c5f4faa70c
mponpol <mponpol@gmail.com>
2022-01-20 07:52:59 +0100

    Day 2 Part One finished

--- 2021/Day 2/script.js
+++ 2021/Day 2/script.js
@@ -1008,12 +1008,8 @@ forward 2
 up 1
 forward 5`
 
-// Convert the string into an array with the split() method, creating an array of substrings. As the entries are separated by a newline character, use its escape sequence to split the string.
-let entriesArr = entries.split("\n");
-
-entriesArr = entriesArr.map(value => value.split(" "));
-
-console.log(entriesArr);
+// Convert the string into an array with the split() method, creating an array of substrings. As the entries are separated by a newline character, use its escape sequence to split the string. Then separate strings from numbers
+let entriesArr = entries.split("\n").map(value => value.split(" "));
 
 let position = [0, 0];
 
@@ -1027,6 +1023,12 @@ let entriesArr2 = entriesArr.map(value => {
     }
 });
 
-console.log(entriesArr2);
-console.log(position);
-console.log(position[0] * position[1]);
+console.log(position[0] * position[1]); // 1427868 (2034 * 702)
+
+/*
+ * DAY 2 (II)
+ * Using this new interpretation of the commands, calculate the horizontal position
+ * and depth you would have after following the planned course. What do you get if
+ * you multiply your final horizontal position by your final depth?
+ */
+
