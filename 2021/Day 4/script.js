0bf669a757cfc7f5669bf93e0136b5c6286387bc
mponpol <mponpol@gmail.com>
2022-02-08 22:30:42 +0100

    Looking for checkBoard function

--- 2021/Day 4/script.js
+++ 2021/Day 4/script.js
@@ -7,7 +7,7 @@ import entries from './input.js';
 
 // Get numbers and boards:
 const entriesArr = entries.split('\n\n');
-const drawNumbers = entriesArr.slice(0, 1);
+const drawNumbers = entriesArr.slice(0, 1).join().split(',');
 const boards = entriesArr.slice(1).map(val => val.split('\n'));
 
 boards.forEach(board => {
@@ -19,15 +19,38 @@ boards.forEach(board => {
   }
 });
 
-// Get a number
 // Check if drawn number is in boards
-// Check rows
-// Check columns
+
+const checkBoard = function (board) {
+  // Check rows
+  for (let i = 0; i < board[i].length; i++) {
+    let aciertos = board[i].filter(x => x === 'x').length;
+    if (aciertos === board[i].length) {
+      break;
+    }
+  }
+  // Check columns
+  for (let j = 0; j < board[i].length; j++) {}
+};
+
+// Get a number
+for (let i = 0; i < drawNumbers.length; i++) {
+  for (let j = 0; j < boards.length; j++) {
+    checkBoard(boards[j]);
+  }
+}
+
+// // Let has local scope
+// let array = [1, 2, 3, 5, 2, 8, 9, 2]
+
+// // Functional filter with an Arrow function
+// array.filter(x => x === 2).length  // -> 3
 
 // Sum of all unmarked numbers on the board
 
 // Multiply that sum by the number that was called when the board won
 
+console.log(typeof drawNumbers);
 console.log(drawNumbers);
 console.log(boards);
 console.log(typeof boards[0]);
