1931bcf0d4e27007c55ea5dc75bfdcf3ed6be340
mponpol <mponpol@gmail.com>
2022-03-06 22:24:12 +0100

    Day 4 Part One new solution

--- 2021/Day 4/script.js
+++ 2021/Day 4/script.js
@@ -9,7 +9,11 @@ import data from './input.js';
 
 // Get numbers and boards:
 const dataArr = data.split('\n\n');
-const drawNumbers = dataArr.slice(0, 1).join().split(',');
+const drawNumbers = dataArr
+  .slice(0, 1)
+  .join()
+  .split(',')
+  .map(x => parseInt(x));
 const boards = dataArr.slice(1).map(val => val.split('\n'));
 
 boards.forEach(board => {
@@ -17,92 +21,227 @@ boards.forEach(board => {
     board[i] = board[i]
       .trim()
       .split(' ')
-      .filter(x => x !== '');
+      .filter(x => x !== '')
+      .map(x => parseInt(x));
   }
 });
 
 let gameOn = true;
-let winner, coef, winnerBall;
+let winner, winnerBall;
 
 const checkBingo = function (ball, cartons) {
   for (let i = 0; i < ball.length; i++) {
     if (gameOn) {
-      checkCartons(ball[i], cartons);
-    }
-  }
-
-  function checkCartons(ball, cartons) {
-    for (let i = 0; i < cartons.length; i++) {
-      checkCarton(ball, cartons[i]);
-    }
-  }
-
-  function checkCarton(ball, carton) {
-    for (let i = 0; i < carton.length; i++) {
-      checkLine(ball, carton[i]);
-      if (!gameOn) {
-        for (let line of carton) {
-          if (line.filter(x => x === 'x').length === 5) {
-            winner = carton;
-            winnerBall = ball;
+      for (let j = 0; j < cartons.length; j++) {
+        for (let k = 0; k < cartons[j].length; k++) {
+          for (let l = 0; l < cartons[j][k].length; l++) {
+            if (ball[i] === cartons[j][k][l]) {
+              cartons[j][k][l] = 'x';
+            }
           }
         }
       }
+      winnerBall = ball[i];
     }
-  }
-
-  // Function to check lines looking for the drawn number
-  function checkLine(ball, line) {
-    for (let i = 0; i < line.length; i++) {
-      if (ball === line[i]) {
-        line[i] = 'x';
+    for (let m = 0; m < cartons.length; m++) {
+      for (let n = 0; n < cartons[m].length; n++) {
+        if (cartons[m][n].every(x => x === 'x')) {
+          winner = cartons[m];
+          gameOn = false;
+        }
       }
     }
-    checkWinningLine(line);
-  }
-
-  // Function to check if there's a winning line and stop the game
-  function checkWinningLine(line) {
-    if (line.filter(x => x === 'x').length === 5) {
-      gameOn = false;
+    for (let p = 0; p < cartons.length; p++) {
+      if (
+        cartons[0][p] === 'x' &&
+        cartons[1][p] === 'x' &&
+        cartons[2][p] === 'x' &&
+        cartons[3][p] === 'x' &&
+        cartons[4][p] === 'x'
+      ) {
+        winner = cartons[m];
+        gameOn = false;
+      }
     }
   }
 };
 
 checkBingo(drawNumbers, boards);
 
-const result = winner.map(arr => arr.filter(x => x !== 'x'));
-for (let i = 0; i < result.length; i++) {
-  for (let j = 0; j < result[i].length; j++) {
-    result[i][j] = parseInt(result[i][j]);
-  }
-  result[i] = result[i].reduce((x, y) => x + y, 0);
+let boardSum = winner.map(arr => arr.filter(x => x !== 'x'));
+for (let i = 0; i < boardSum.length; i++) {
+  boardSum[i] = boardSum[i].reduce((x, y) => x + y, 0);
 }
-coef = result.reduce((x, y) => x + y, 0);
+boardSum = boardSum.reduce((x, y) => x + y, 0);
 
-console.log(coef);
+console.log(boardSum);
 console.log(winnerBall);
-console.log(coef * winnerBall); // 41668 (947 * 44)
-console.log(boards);
+console.log(boardSum * winnerBall); // 41668 (947 * 44)
+
+// // Get numbers and boards:
+// const dataArr = data.split('\n\n');
+// const drawNumbers = dataArr
+//   .slice(0, 1)
+//   .join()
+//   .split(',')
+//   .map(x => parseInt(x));
+// const boards = dataArr.slice(1).map(val => val.split('\n'));
+
+// boards.forEach(board => {
+//   for (let i = 0; i < board.length; i++) {
+//     board[i] = board[i]
+//       .trim()
+//       .split(' ')
+//       .filter(x => x !== '')
+//       .map(x => parseInt(x));
+//   }
+// });
+
+// let gameOn = true;
+// let winner, winnerBall;
+
+// const checkBingo = function (ball, cartons) {
+//   for (let i = 0; i < ball.length; i++) {
+//     if (gameOn) {
+//       checkCartons(ball[i], cartons);
+//     }
+//   }
+
+//   function checkCartons(ball, cartons) {
+//     for (let i = 0; i < cartons.length; i++) {
+//       checkCarton(ball, cartons[i]);
+//     }
+//   }
+
+//   function checkCarton(ball, carton) {
+//     for (let i = 0; i < carton.length; i++) {
+//       checkLine(ball, carton[i]);
+//       if (!gameOn) {
+//         for (let line of carton) {
+//           if (line.filter(x => x === 'x').length === 5) {
+//             winner = carton;
+//             winnerBall = ball;
+//           }
+//         }
+//       }
+//       checkWinningCol(carton[i]);
+//     }
+//   }
+
+//   // Function to check lines looking for the drawn number
+//   function checkLine(ball, line) {
+//     for (let i = 0; i < line.length; i++) {
+//       if (ball === line[i]) {
+//         line[i] = 'x';
+//       }
+//     }
+//     checkWinningLine(line);
+//   }
+
+//   // Function to check if there's a winning line and stop the game
+//   function checkWinningLine(line) {
+//     if (line.every(x => x === 'x')) {
+//       gameOn = false;
+//     }
+//   }
+
+//   // Function to check if there's a winning column and stop the game
+//   function checkWinningCol(carton) {
+//     for (let i = 0; i < 5; i++) {
+//       if (
+//         carton[0][i] === 'x' &&
+//         carton[1][i] === 'x' &&
+//         carton[2][i] === 'x' &&
+//         carton[3][i] === 'x' &&
+//         carton[4][i] === 'x'
+//       ) {
+//         gameOn = false;
+//       }
+//     }
+//   }
+// };
+
+// checkBingo(drawNumbers, boards);
+
+// // Calculate the sum of the unmarked numbers on the winning board
+// let boardSum = winner.map(arr => arr.filter(x => x !== 'x'));
+// for (let i = 0; i < boardSum.length; i++) {
+//   boardSum[i] = boardSum[i].reduce((x, y) => x + y, 0);
+// }
+// boardSum = boardSum.reduce((x, y) => x + y, 0);
+
+// console.log(boardSum);
+// console.log(winnerBall);
+// console.log(boardSum * winnerBall); // 41668 (947 * 44)
+// console.log(boards);
+
 /*
 DAY 4 (II)
 Figure out which board will win last. Once it wins, what would its final score be?
  */
 
-let game2On = true;
+// let game2On = true;
+// let winnerBoards = [];
 
-const checkLastBingo = function (ball, cartons) {
-  for (let i = drawNumbers.indexOf(winnerBall); i < ball.length; i++) {
-    if (game2On) {
-      checkCartons(ball[i], cartons);
-    }
-  }
+// const checkLastBingo = function (ball, cartons) {
+//   for (let i = drawNumbers.indexOf(winnerBall); i < ball.length; i++) {
+//     // deleteWinningBoards(cartons);
 
-  function checkCartons(ball, cartons) {
-    for (let i = 0; i < cartons.length; i++) {
-      checkCarton(ball, cartons[i]);
-    }
-  }
-};
+//     checkCartons(ball[i], cartons);
+//   }
 
-checkLastBingo(drawNumbers, boards);
+//   // function deleteWinningBoards(boards) {
+
+//   // }
+
+//   function checkCartons(ball, cartons) {
+//     for (let i = 0; i < cartons.length; i++) {
+//       checkCarton(ball, cartons[i]);
+//     }
+//   }
+
+//   function checkCarton(ball, carton) {
+//     for (let i = 0; i < carton.length; i++) {
+//       if (checkWinningCarton(carton)) {
+//         checkLine(ball, carton[i]);
+//       }
+//       checkWinningCol(carton[i]);
+//     }
+//   }
+
+//   // Function to check lines looking for the drawn number
+//   function checkLine(ball, line) {
+//     for (let i = 0; i < line.length; i++) {
+//       if (ball === line[i]) {
+//         line[i] = 'x';
+//       }
+//     }
+//   }
+
+//   // Function to check if there's a winning column and stop the game
+//   function checkWinningCol(carton) {
+//     for (let i = 0; i < 5; i++) {
+//       if (
+//         carton[0][i] === 'x' &&
+//         carton[1][i] === 'x' &&
+//         carton[2][i] === 'x' &&
+//         carton[3][i] === 'x' &&
+//         carton[4][i] === 'x'
+//       ) {
+//         gameOn = false;
+//       }
+//     }
+//   }
+
+//   function checkWinningCarton(carton) {
+//     for (let line of carton) {
+//       if (line.every(x => x === 'x')) {
+//         return false;
+//       }
+//     }
+//   }
+// };
+
+// checkLastBingo(drawNumbers, boards);
+
+// console.log(winnerBoards);
