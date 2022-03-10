a0ee6753515ea7aa582961d666d306fc738c5cff
mponpol <mponpol@gmail.com>
2022-03-10 12:07:55 +0100

    start Day 5

---
+++ 2021/Day 5/script.js
@@ -0,0 +1,8 @@
+/*
+DAY 5 (I)
+To avoid the most dangerous areas, you need to determine the number of points where at least two lines overlap. In the above example, this is anywhere in the diagram with a 2 or larger - a total of 5 points.
+
+Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
+ */
+
+import data from './input.js';
