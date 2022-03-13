a6736410edca7dae8e5c36e38e00040cdd626db0
mponpol <mponpol@gmail.com>
2022-03-13 21:53:07 +0100

    get horizontal and vertical lines

--- 2021/Day 5/script.js
+++ 2021/Day 5/script.js
@@ -16,11 +16,62 @@ const vents = data.split('\n').map(x =>
     .map(Number)
 );
 
-// Extract horizontal and vertical lines (x1 = x2 or y1 = y2)
-const linesHorVer = [];
+// Extract horizontal (y1 = y2) and vertical lines (x1 = x2)
+const linesHor = []; // 147 horizontal lines
+const linesVer = []; // 168 vertical lines
 for (let [x1, y1, x2, y2] of vents) {
-  if (x1 === x2 || y1 === y2) {
-    linesHorVer.push([x1, y1, x2, y2]);
+  if (x1 === x2) {
+    y1 > y2 ? linesVer.push([x1, y2, x2, y1]) : linesVer.push([x1, y1, x2, y2]);
+  }
+  if (y1 === y2) {
+    x1 > x2 ? linesHor.push([x2, y2, x1, y1]) : linesHor.push([x1, y1, x2, y2]);
   }
 }
-console.log(linesHorVer);
+console.log(linesHor);
+console.log(linesHor[0]);
+console.log(linesVer);
+console.log(linesVer[0]);
+
+// Iterate over all horizontal lines and check crosses with vertical lines and overlaps with other horizontal lines
+let crosses = 0;
+for (let [x1, y1, x2, y2] of linesHor) {
+  for (let [a1, b1, a2, b2] of linesVer) {
+    if (x1 <= a1 && a1 <= x2 && b1 <= y1 && y1 <= b2) {
+      crosses++;
+    }
+  }
+  for (let [a1, b1, a2, b2] of linesHor) {
+    let overlap = 0;
+    if (y1 === b1) {
+      if (a1 <= x1 && a2 <= x2) {
+        overlap = a2 - x1;
+      } else if (a1 <= x2 && x2 <= a2) {
+        overlap = x2 - a1;
+      } else if (x1 <= a1 && a2 <= x2) {
+        overlap = a2 - a1;
+      }
+    }
+    crosses += overlap;
+  }
+}
+for (let [x1, y1, x2, y2] of linesVer) {
+  for (let [a1, b1, a2, b2] of linesVer) {
+    let overlap = 0;
+    if (x1 === a1) {
+      if (b1 <= y1 && y1 <= b2 && b2 <= y2) {
+        overlap = b2 - y1;
+      } else if (b1 >= y1 && y2 <= b2 && b1 <= y2) {
+        overlap = y2 - b1;
+      } else if (y1 <= b1 && b2 <= b2) {
+        overlap = b2 - b1;
+      }
+    }
+    crosses += overlap;
+  }
+}
+
+console.log(crosses); // 2729 crosses is too low (24696 possibilities)
+// 52768 is wrong
+// 53695 is wrong
+// 108252 is wrong
+// 108588 is wrong
