ea769d2363cd1f5aa2539ac2ee81a2da4689f6f5
mponpol <mponpol@gmail.com>
2022-03-17 17:02:08 +0100

    start Day 6

---
+++ 2021/Day 6/script.js
@@ -0,0 +1,10 @@
+/*
+DAY 6 (I)
+Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each other number decreases by 1 if it was present at the start of the day.
+
+In this example, after 18 days, there are a total of 26 fish. After 80 days, there would be a total of 5934.
+
+Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?
+ */
+
+import data from './inputTest.js';
