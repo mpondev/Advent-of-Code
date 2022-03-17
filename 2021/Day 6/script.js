3d7c313beaea1199a3b4800f5b81c36968de00fb
mponpol <mponpol@gmail.com>
2022-03-17 20:39:44 +0100

    start Day 6 Part Two

--- 2021/Day 6/script.js
+++ 2021/Day 6/script.js
@@ -7,4 +7,29 @@ In this example, after 18 days, there are a total of 26 fish. After 80 days, the
 Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?
  */
 
-import data from './inputTest.js';
+import data from './input.js';
+
+const lanternfish = [];
+for (let value of data) {
+  if (value !== ',') {
+    lanternfish.push(Number(value));
+  }
+}
+
+for (let i = 0; i < 80; i++) {
+  for (let j = 0; j < lanternfish.length; j++) {
+    if (lanternfish[j] > 0) {
+      lanternfish[j]--;
+    } else if (lanternfish[j] === 0) {
+      lanternfish[j] = 6;
+      lanternfish.push(9);
+    }
+  }
+}
+
+console.log(lanternfish.length); // 349549
+
+/*
+DAY 6 (II)
+How many lanternfish would there be after 256 days?
+ */
