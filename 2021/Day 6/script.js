213dd27977a9d1444722b5ed422599549f12ae1a
mponpol <mponpol@gmail.com>
2022-03-31 07:44:26 +0200

    part Two new Map point of view

--- 2021/Day 6/script.js
+++ 2021/Day 6/script.js
@@ -7,14 +7,22 @@ In this example, after 18 days, there are a total of 26 fish. After 80 days, the
 Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?
  */
 
-import data from './input.js';
+import data from './inputTest.js';
 
-const lanternfish = [];
-for (let value of data) {
-  if (value !== ',') {
-    lanternfish.push(Number(value));
-  }
+const lanternfish = data.split(',').map(x => Number(x));
+
+const population = new Map();
+for (let i = 0; i <= 8; i++) {
+  population.set(i, 0);
 }
+for (let i = 0; i < lanternfish.length; i++) {
+  population.forEach((val, key) => {
+    if (key === lanternfish[i]) {
+      val++;
+    }
+  });
+}
+console.log(population);
 
 for (let i = 0; i < 80; i++) {
   for (let j = 0; j < lanternfish.length; j++) {
@@ -31,5 +39,61 @@ console.log(lanternfish.length); // 349549
 
 /*
 DAY 6 (II)
-How many lanternfish would there be after 256 days?
+How many lanternfish would there be after 256 days? 26984457539 in the example.
  */
+
+// Solution: 1589590444365 ??
+
+function growFish(input, numberOfDays) {
+  let fishByAge = populateAges(input);
+
+  for (let day = 0; day < numberOfDays; day++) {
+    const todayAges = initNumberMap(0, 8);
+
+    // age the non-reproducers
+    for (let age = 1; age <= 8; age++) {
+      todayAges.set(age - 1, fishByAge.get(age));
+    }
+
+    // handle the day's reproducers
+    incrementMapElement(todayAges, 8, fishByAge.get(0));
+    incrementMapElement(todayAges, 6, fishByAge.get(0));
+
+    fishByAge = todayAges;
+  }
+
+  // return fish count
+  return [...fishByAge.values()].reduce((acc, value) => acc + value);
+}
+
+function populateAges(input) {
+  const fishMap = initNumberMap(0, 8);
+
+  for (let i = 0; i < input.length; i++) {
+    incrementMapElement(fishMap, input[i], 1);
+  }
+
+  return fishMap;
+}
+
+function incrementMapElement(map, key, amount) {
+  map.set(key, map.get(key) + amount);
+}
+
+// Crea y devuelve un Map
+function initNumberMap(start, end) {
+  const newMap = new Map();
+  for (let i = start; i <= end; i++) {
+    newMap.set(i, 0);
+  }
+  return newMap;
+}
+
+const input = data.split(',').map(x => parseInt(x));
+
+console.log(growFish(input, 80));
+console.log(growFish(input, 256));
+
+console.log(initNumberMap(0, 8));
+const todayAges = initNumberMap(0, 8);
+console.log(todayAges);
