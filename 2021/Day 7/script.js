a13332fb7d4ed0a2cf415ae924b97b855685d78f
mponpol <mponpol@gmail.com>
2022-03-31 22:14:49 +0200

    start Day 7

---
+++ 2021/Day 7/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 7 (I)
+Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?
+ */
+
+import data from './input.js';
+
+console.log(data);
