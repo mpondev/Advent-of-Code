164158a351746991ce532d25713100d054e33fd2
mponpol <mponpol@gmail.com>
2022-04-28 22:38:06 +0200

    start Day 9

---
+++ 2021/Day 9/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+DAY 9 (I)
+Find all of the low points on your heightmap. What is the sum of the risk levels of all low points on your heightmap?
+ */
+
+import data from './input.js';
+
+/*
+DAY 9 (II)
+ */
