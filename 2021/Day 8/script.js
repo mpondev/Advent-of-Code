4e0c22c5e3be95900e3dd3cac55fbda8e15a9941
mponpol <mponpol@gmail.com>
2022-04-28 22:27:46 +0200

    finish Day 8

--- 2021/Day 8/script.js
+++ 2021/Day 8/script.js
@@ -5,7 +5,7 @@ DAY 8 (I)
 In the output values, how many times do digits 1, 4, 7, or 8 appear?
  */
 
-import data from './inputTest.js';
+import data from './input.js';
 
 const outputs = data
   .split('\n')
@@ -24,35 +24,20 @@ const counter = function (array) {
   return count;
 };
 
-console.log(counter(outputs)); // 445 (26 for the test)
-
-// acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
-//    8      *     *     *    7    *      *     4     *    1
-//    8    2/3/5 2/3/5 2/3/5  7  0/6/9  0/6/9   4   0/6/9  1
-//    8      5     2     3    7    9      6     4     0    1
+console.log(counter(outputs)); // 445 (Test: 26)
 
 /*
-1 ->  ,  , c,  ,  , f,   [2] 1º
-7 -> a,  , c,  ,  , f,   [3] 2º
-4 ->  , b, c, d,  , f,   [4] 3º
-2 -> a,  , c, d, e,  , g [5] 10º el último
-3 -> a,  , c, d,  , f, g [5] 8º contiene todas las de 7 (2º)
-5 -> a, b,  , d,  , f, g [5] 9º le falta una de 6 (7º), la e
-0 -> a, b, c,  , e, f, g [6] 6º contiene todas las de 7 (2º) y obtiene la d restando de 8 (4º)
-6 -> a, b,  , d, e, f, g [6] 7º único con 6 caracteres restante, obtiene la c restando de 8 (4º)
-9 -> a, b, c, d,  , f, g [6] 5º (7 + 4 + g) contiene todas las de 7 (2º) y las de 4 (3º); sobra g
-8 -> a, b, c, d, e, f, g [7] 4º
-*/
-
+DAY 8 (II)
+For each entry, determine all of the wire/segment connections and decode the four-digit output values. What do you get if you add up all of the output values?
+ */
 const inputs = data
   .split('\n')
   .map(x => x.split(' | '))
   .map(x => x.splice(0, 1).join())
   .map(x => x.split(' '));
 
-console.log(inputs);
-
 const findoutInputs = function (inp, out) {
+  // Function that receives 2 Sets, compares them and returns the different letter(s)
   const missingLetter = function (set1, set2) {
     let missing = new Set([...set1]);
     for (let elem of new Set([...set2])) {
@@ -63,6 +48,7 @@ const findoutInputs = function (inp, out) {
     return letter;
   };
 
+  // Function that receives an String and returns a Set
   const sendLettersToSet = function (str, set) {
     for (let letter of str) {
       set.add(letter);
@@ -80,52 +66,55 @@ const findoutInputs = function (inp, out) {
       g: '',
     };
 
-    const digit0 = new Set(); // a, b, c,  , e, f, g [6] *
-    const digit1 = new Set(); //  ,  , c,  ,  , f,   [2] *
-    const digit2 = new Set(); // a,  , c, d, e,  , g [5]
-    const digit3 = new Set(); // a,  , c, d,  , f, g [5] *
-    const digit4 = new Set(); //  , b, c, d,  , f,   [4] *
-    const digit5 = new Set(); // a, b,  , d,  , f, g [5] *
-    const digit6 = new Set(); // a, b,  , d, e, f, g [6] *
-    const digit7 = new Set(); // a,  , c,  ,  , f,   [3] *
-    const digit8 = new Set(); // a, b, c, d, e, f, g [7] *
-    const digit9 = new Set(); // a, b, c, d,  , f, g [6] *
+    const digit0 = new Set(); // a, b, c,  , e, f, g [length: 6]
+    const digit1 = new Set(); //  ,  , c,  ,  , f,   [length: 2]
+    // const digit2 = new Set(); // a,  , c, d, e,  , g [length: 5]
+    // const digit3 = new Set(); // a,  , c, d,  , f, g [length: 5]
+    const digit4 = new Set(); //  , b, c, d,  , f,   [length: 4]
+    // const digit5 = new Set(); // a, b,  , d,  , f, g [length: 5]
+    const digit6 = new Set(); // a, b,  , d, e, f, g [length: 6]
+    const digit7 = new Set(); // a,  , c,  ,  , f,   [length: 3]
+    const digit8 = new Set(); // a, b, c, d, e, f, g [length: 7]
+    const digit9 = new Set(); // a, b, c, d,  , f, g [length: 6]
 
     for (let j = 0; j < inp[i].length; j++) {
-      if (inp[i][j].length === 2) sendLettersToSet(inp[i][j], digit1);
-      if (inp[i][j].length === 3) sendLettersToSet(inp[i][j], digit7);
-      if (inp[i][j].length === 4) sendLettersToSet(inp[i][j], digit4);
-      if (inp[i][j].length === 7) sendLettersToSet(inp[i][j], digit8);
+      if (inp[i][j].length === 2) {
+        sendLettersToSet(inp[i][j], digit1);
+      } else if (inp[i][j].length === 3) {
+        sendLettersToSet(inp[i][j], digit7);
+      } else if (inp[i][j].length === 4) {
+        sendLettersToSet(inp[i][j], digit4);
+      } else if (inp[i][j].length === 7) {
+        sendLettersToSet(inp[i][j], digit8);
+      }
+      segments.a = missingLetter(digit7, digit1);
     }
 
     for (let j = 0; j < inp[i].length; j++) {
-      let tempDigit = new Set([...digit7, ...digit4]);
+      let tempDigit1 = new Set([...digit7, ...digit4]);
       if (inp[i][j].length === 6) {
         let counter = 0;
         for (let letter of inp[i][j]) {
-          if (tempDigit.has(letter)) counter++;
+          if (tempDigit1.has(letter)) counter++;
         }
         if (counter === 5) {
           sendLettersToSet(inp[i][j], digit9);
         }
+        segments.g = missingLetter(digit9, tempDigit1);
       }
-      segments.g = missingLetter(digit9, tempDigit);
-    }
 
-    for (let j = 0; j < inp[i].length; j++) {
-      let tempDigit = new Set([...digit7]);
+      let tempDigit2 = new Set([...digit7]);
       if (inp[i][j].length === 6 && inp[i][j] !== [...digit9].join('')) {
         let counter = 0;
         for (let letter of inp[i][j]) {
-          if (tempDigit.has(letter)) counter++;
+          if (tempDigit2.has(letter)) counter++;
         }
         if (counter === 3) {
           sendLettersToSet(inp[i][j], digit0);
         }
+        segments.d = missingLetter(digit8, digit0);
       }
-    }
 
-    for (let j = 0; j < inp[i].length; j++) {
       if (
         inp[i][j].length === 6 &&
         inp[i][j] !== [...digit9].join('') &&
@@ -133,93 +122,45 @@ const findoutInputs = function (inp, out) {
       ) {
         sendLettersToSet(inp[i][j], digit6);
       }
+      segments.c = missingLetter(digit8, digit6);
+      segments.f = missingLetter(digit1, segments.c);
+      let temp = new Set([segments.c, segments.d, segments.f]);
+      segments.b = missingLetter(digit4, temp);
+      temp = new Set([
+        segments.a,
+        segments.b,
+        segments.c,
+        segments.d,
+        segments.f,
+        segments.g,
+      ]);
+      segments.e = missingLetter(digit8, temp);
     }
 
-    for (let j = 0; j < inp[i].length; j++) {
-      let tempDigit = new Set([...digit7]);
-      if (inp[i][j].length === 5) {
-        let counter = 0;
-        for (let letter of inp[i][j]) {
-          if (tempDigit.has(letter)) counter++;
-        }
-        if (counter === 3) {
-          sendLettersToSet(inp[i][j], digit3);
-        }
-      }
-    }
-
-    for (let j = 0; j < inp[i].length; j++) {
-      let tempDigit = new Set([...digit6]);
-      if (inp[i][j].length === 5 && inp[i][j] !== [...digit6].join('')) {
-        let counter = 0;
-        for (let letter of inp[i][j]) {
-          if (tempDigit.has(letter)) counter++;
-        }
-        if (counter === 5) {
-          sendLettersToSet(inp[i][j], digit5);
-        }
-      }
-    }
-
-    segments.a = missingLetter(digit7, digit1);
-    segments.b = missingLetter(digit9, digit3);
-    segments.c = missingLetter(digit8, digit6);
-    segments.d = missingLetter(digit8, digit0);
-    segments.e = missingLetter(digit8, digit9);
-
-    let letters = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g']);
-    for (let value of Object.values(segments)) {
-      letters.delete(value);
-    }
-    segments.f = [...letters].toString();
-
-    console.log(segments);
-
-    for (let i = 0; i < out.length; i++) {
-      for (let j = 0; j < out[i].length; j++) {
-        if (out[i][j].length === 2) out[i][j] = 1;
-        if (out[i][j].length === 3) out[i][j] = 7;
-        if (out[i][j].length === 4) out[i][j] = 4;
-        if (out[i][j].length === 7) out[i][j] = 8;
-        if (out[i][j].length === 6 && !out[i][j].includes(segments.e))
-          out[i][j] = 9;
-        if (out[i][j].length === 6 && !out[i][j].includes(segments.c))
-          out[i][j] = 6;
-        if (out[i][j].length === 6 && !out[i][j].includes(segments.d))
-          out[i][j] = 0;
-        if (
-          out[i][j].length === 5 &&
-          !out[i][j].includes(segments.c) &&
-          !out[i][j].includes(segments.e)
-        )
-          out[i][j] = 5;
-        if (
-          out[i][j].length === 5 &&
-          !out[i][j].includes(segments.b) &&
-          !out[i][j].includes(segments.e)
-        )
-          out[i][j] = 3;
-        if (
-          out[i][j].length === 5 &&
-          !out[i][j].includes(segments.b) &&
-          !out[i][j].includes(segments.f)
-        )
-          out[i][j] = 2;
-      }
+    for (let j = 0; j < out[i].length; j++) {
+      if (out[i][j].length === 2) out[i][j] = 1;
+      if (out[i][j].length === 3) out[i][j] = 7;
+      if (out[i][j].length === 4) out[i][j] = 4;
+      if (out[i][j].length === 7) out[i][j] = 8;
+      if (out[i][j].length === 6 && !out[i][j].includes(segments.d))
+        out[i][j] = 0;
+      if (out[i][j].length === 6 && !out[i][j].includes(segments.c))
+        out[i][j] = 6;
+      if (out[i][j].length === 6 && !out[i][j].includes(segments.e))
+        out[i][j] = 9;
+      if (out[i][j].length === 5 && out[i][j].includes(segments.e))
+        out[i][j] = 2;
+      if (out[i][j].length === 5 && out[i][j].includes(segments.b))
+        out[i][j] = 5;
+      if (out[i][j].length === 5) out[i][j] = 3;
     }
   }
-
   const solution = [];
   for (let el of out) {
     solution.push(Number(el.join('')));
   }
   const answer = solution.reduce((a, b) => a + b, 0);
-  console.log(solution, answer);
+  console.log(answer);
 };
 
-findoutInputs(inputs, outputs); // 988760 too low (61228 for the test)
-
-console.log(outputs);
-console.log(outputs[0].join(''));
-let aaa = [8, 3, 9, 4];
-console.log(aaa.join(''));
+findoutInputs(inputs, outputs); // 1043101 (Test: 61229)
