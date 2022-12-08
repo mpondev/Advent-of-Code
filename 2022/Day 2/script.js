e28199fb9cb0058b78335c791f5221e5c4bb8c7a
mponpol <mponpol@gmail.com>
2022-12-08 09:04:02 +0100

    finish Day 2 (2022)

--- 2022/Day 2/script.js
+++ 2022/Day 2/script.js
@@ -7,19 +7,83 @@ What would your total score be if everything goes exactly according to your stra
 
 import data from './input.js';
 
-const caloriesBags = data
-  .split('\n\n')
-  .map(el => el.split('\n').reduce((a, b) => parseInt(a) + parseInt(b), 0))
-  .sort((a, b) => b - a);
+// A: Rock, B: Paper, C: Scissors
+// X: Rock, Y: Paper, Z: Scissors
+// 1: Rock, 2: Paper, and 3: Scissors
+// 0: lost, 3: draw, 6: won
 
-const maxCaloriesBag = caloriesBags[0];
+const rounds = data.split('\n').map(el => el.split(' '));
 
-console.log(maxCaloriesBag); // (Test: 15)
+const [win, draw, lost] = [6, 3, 0];
+const [rock, paper, scissors] = [1, 2, 3];
+
+const getTotalScore = function (rounds) {
+  let totalScore = 0;
+  for (let round of rounds) {
+    let score = 0;
+    if (round[1] === 'X') {
+      round[0] === 'A'
+        ? (score += rock + draw)
+        : round[0] === 'B'
+        ? (score += rock + lost)
+        : (score += rock + win);
+    } else if (round[1] === 'Y') {
+      round[0] === 'A'
+        ? (score += paper + win)
+        : round[0] === 'B'
+        ? (score += paper + draw)
+        : (score += paper + lost);
+    } else if (round[1] === 'Z') {
+      round[0] === 'A'
+        ? (score += scissors + lost)
+        : round[0] === 'B'
+        ? (score += scissors + win)
+        : (score += scissors + draw);
+    }
+    totalScore += score;
+  }
+  return totalScore;
+};
+
+console.log(`Total Score: ${getTotalScore(rounds)}`); // 10404 (Test: 15)
 
 /*
 DAY 2 (II)
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
  */
 
-const top3Bags = caloriesBags.slice(0, 3).reduce((a, b) => a + b, 0);
+// X: lose, Y: draw, Z: win
+// A: Rock, B: Paper, C: Scissors
+// X: Rock, Y: Paper, Z: Scissors
+// 1: Rock, 2: Paper, and 3: Scissors
+// 0: lost, 3: draw, 6: won
 
-console.log(top3Bags); // 203420 Too low (Test: 45000)
+const getTotalScore2 = function (rounds) {
+  let totalScore = 0;
+  for (let round of rounds) {
+    let score = 0;
+    if (round[1] === 'X') {
+      round[0] === 'A'
+        ? (score += scissors + lost)
+        : round[0] === 'B'
+        ? (score += rock + lost)
+        : (score += paper + lost);
+    } else if (round[1] === 'Y') {
+      round[0] === 'A'
+        ? (score += rock + draw)
+        : round[0] === 'B'
+        ? (score += paper + draw)
+        : (score += scissors + draw);
+    } else if (round[1] === 'Z') {
+      round[0] === 'A'
+        ? (score += paper + win)
+        : round[0] === 'B'
+        ? (score += scissors + win)
+        : (score += rock + win);
+    }
+    totalScore += score;
+  }
+  return totalScore;
+};
+
+console.log(`Total Score: ${getTotalScore2(rounds)}`); // 10334 (Test: 12)
