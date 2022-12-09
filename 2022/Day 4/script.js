7625443f2e13ee9e7c5db689c5d86ddd12ac0e5f
mponpol <mponpol@gmail.com>
2022-12-09 13:03:59 +0100

    start Day 4 part Two (2022)

--- 2022/Day 4/script.js
+++ 2022/Day 4/script.js
@@ -7,4 +7,40 @@ In how many assignment pairs does one range fully contain the other?
 
 import data from './input.js';
 
-console.log(data);
+const sections = data
+  .split('\n')
+  .map(el => el.split(',').map(el => el.split('-').map(el => parseInt(el))));
+
+function checkRanges(sections) {
+  let counter = 0;
+  for (let section of sections) {
+    if (
+      (section[0][0] >= section[1][0] && section[0][1] <= section[1][1]) ||
+      (section[0][0] <= section[1][0] && section[0][1] >= section[1][1])
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+console.log(checkRanges(sections)); // 540 (Test: 2)
+
+/*
+DAY 4 (II)
+In how many assignment pairs do the ranges overlap?
+ */
+function checkRanges2(sections) {
+  let counter = 0;
+  for (let section of sections) {
+    if (
+      (section[0][0] <= section[1][0] && section[0][1] >= section[1][0]) ||
+      (section[1][0] <= section[0][0] && section[1][1] >= section[0][0])
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+console.log(checkRanges2(sections)); // 872 (Test: 4)
