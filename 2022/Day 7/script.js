e28e041de874493be21715c8aaaf65b92bee58bd
mponpol <mponpol@gmail.com>
2022-12-11 20:21:01 +0100

    start Day 7 (2022)

---
+++ 2022/Day 7/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 7 (I)
+Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
+ */
+
+import data from './inputTest.js';
+
+console.log(data);
