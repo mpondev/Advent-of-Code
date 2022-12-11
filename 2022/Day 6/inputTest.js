455649941d3b81a37e314e471b85f31a5adcee20
mponpol <mponpol@gmail.com>
2022-12-11 18:31:15 +0100

    start Day 6(2022)

---
+++ 2022/Day 6/inputTest.js
@@ -0,0 +1,6 @@
+export { test1, test2, test3, test4, test5 };
+const test1 = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`;
+const test2 = `bvwbjplbgvbhsrlpgdmjqwftvncz`;
+const test3 = `nppdvjthqldpwncqszvftbrmjlhg`;
+const test4 = `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`;
+const test5 = `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`;
---
+++ 2022/Day 6/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+DAY 6 (I)
+How many characters need to be processed before the first start-of-packet marker is detected?
+ */
+
+import { test1, test2, test3, test4, test5 } from './inputTest.js';
+
+console.log(test1); // (Test: 7)
+console.log(test2); // (Test: 5)
+console.log(test3); // (Test: 6)
+console.log(test4); // (Test: 10)
+console.log(test5); // (Test: 11)
