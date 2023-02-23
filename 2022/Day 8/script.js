81764746fbb5819bb381ae15b12e702146d34908
mponpol <mponpol@gmail.com>
2023-02-23 11:35:38 +0100

    start Day 8 (2022)

---
+++ 2022/Day 8/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*
+DAY 8 (I)
+Consider your map; how many trees are visible from outside the grid?
+ */
+
+import data from './inputTest.js';
+
+const input = data.split('\n');
+console.log(input);
+
+// (Tests: 21)
