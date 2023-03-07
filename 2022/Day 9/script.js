7633a1704a1cf6d39684ba5d3e072dd67047588d
mponpol <mponpol@gmail.com>
2023-03-07 17:57:22 +0100

    Start Day 9 (2022)

---
+++ 2022/Day 9/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 9 (I)
+Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at least once?
+ */
+
+import data from './input.js';
+
+// (Tests: 13)
