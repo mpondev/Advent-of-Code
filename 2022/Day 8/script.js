2c04ca4d8ca2fe4c4853332a8fa0db87a9085cd2
mponpol <mponpol@gmail.com>
2023-03-07 12:17:45 +0100

    start Day 8 part Two (2022)

--- 2022/Day 8/script.js
+++ 2022/Day 8/script.js
@@ -5,9 +5,60 @@ DAY 8 (I)
 Consider your map; how many trees are visible from outside the grid?
  */
 
-import data from './inputTest.js';
+import data from './input.js';
 
-const input = data.split('\n');
-console.log(input);
+const grid = data.split('\n').map(val => val.split('').map(num => +num));
 
-// (Tests: 21)
+function findVisible(grid) {
+  const visible = new Array(grid.length);
+  let prevMax;
+  for (let row = 0; row < grid.length; row++) {
+    visible[row] = new Array(grid[row].length);
+    prevMax = -1;
+    for (let col = 0; col < grid[row].length; col++) {
+      if (grid[row][col] > prevMax) {
+        visible[row][col] = 1;
+        prevMax = grid[row][col];
+      }
+    }
+    prevMax = -1;
+    for (let col = grid[row].length - 1; col >= 0; col--) {
+      if (grid[row][col] > prevMax) {
+        visible[row][col] = 1;
+        prevMax = grid[row][col];
+      }
+    }
+  }
+
+  for (let col = 0; col < grid[0].length; col++) {
+    prevMax = -1;
+    for (let row = 0; row < grid.length; row++) {
+      if (grid[row][col] > prevMax) {
+        visible[row][col] = 1;
+        prevMax = grid[row][col];
+      }
+    }
+    prevMax = -1;
+    for (let row = grid.length - 1; row >= 0; row--) {
+      if (grid[row][col] > prevMax) {
+        visible[row][col] = 1;
+        prevMax = grid[row][col];
+      }
+    }
+  }
+
+  let visibleTrees = 0;
+  visible.map(row =>
+    row.map(col => {
+      if (col) visibleTrees++;
+    })
+  );
+  console.log(visibleTrees);
+}
+
+findVisible(grid); // 1835 (Tests: 21)
+
+/*
+DAY 8 (II)
+Consider each tree on your map. What is the highest scenic score possible for any tree?
+ */
