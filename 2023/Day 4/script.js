310ac892b99d0e531a1f123ade5bae4898869f05
Mario Ponce <mario@mpondev.com>
2023-12-12 20:10:54 +0100

    finish Day 4 (2023)

--- 2023/Day 4/script.js
+++ 2023/Day 4/script.js
@@ -16,12 +16,18 @@ const cards = data
 
 let points = 0;
 
-for (let card of cards) {
+function getMatches(card) {
   let matches = 0;
 
   for (let number of card[1]) {
     card[0].includes(number) && matches++;
   }
+
+  return matches;
+}
+
+for (let card of cards) {
+  let matches = getMatches(card);
 
   matches === 1 && points++;
   matches > 1 && (points += Math.pow(2, matches - 1));
@@ -33,3 +39,32 @@ console.log(points); // 24706 (Test: 13)
 DAY 4 (II)
 Including the original set of scratchcards, how many total scratchcards do you end up with?
  */
+
+let scratchcards = [];
+let verified = [];
+
+for (let [cardIndex, card] of cards.entries()) {
+  verified.push(cardIndex);
+
+  let matches = getMatches(card);
+
+  if (matches > 0) {
+    for (let i = cardIndex + 1; i <= cardIndex + matches; i++) {
+      scratchcards.push(i);
+    }
+  }
+}
+
+for (let cardNumber of scratchcards) {
+  verified.push(cardNumber);
+
+  let matches = getMatches(cards[cardNumber]);
+
+  if (matches > 0) {
+    for (let i = cardNumber + 1; i <= cardNumber + matches; i++) {
+      scratchcards.push(i);
+    }
+  }
+}
+
+console.log(verified.length); // 13114317 (Test: 30)
