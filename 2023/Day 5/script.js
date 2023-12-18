b7a7b47baf3641129fe9f0f92d9ea4458462d548
Mario Ponce <mario@mpondev.com>
2023-12-18 18:47:22 +0100

    finish Day 5 part One (2023)

--- 2023/Day 5/script.js
+++ 2023/Day 5/script.js
@@ -7,4 +7,41 @@ What is the lowest location number that corresponds to any of the initial seed n
 
 import data from './input.js';
 
-// (Test: 35)
+const almanac = data
+  .split(/\n\n/)
+  .map(el => el.split(':'))
+  .map(el => el.map(el => el.trim().split('\n')).flat())
+  .map(el => el.map(el => el.split(' ')).slice(1));
+
+const seeds = almanac[0][0].map(el => Number(el));
+const maps = almanac
+  .slice(1)
+  .map(group => group.map(line => line.map(el => Number(el))));
+
+function convert(seed, maps) {
+  let newSeed = seed;
+
+  for (let map of maps) {
+    let mapConversion;
+
+    for (let line of map) {
+      let range = line[2];
+      let diff = line[0] - line[1];
+
+      if (line[1] <= newSeed && newSeed <= line[1] + range && !mapConversion) {
+        mapConversion = newSeed + diff;
+        newSeed += diff;
+      }
+    }
+  }
+
+  return newSeed;
+}
+
+const result = [];
+
+for (let seed of seeds) {
+  result.push(convert(seed, maps));
+}
+
+console.log(Math.min(...result)); // 346433842 (Test: 35)
