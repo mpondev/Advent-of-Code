b760b0883d6ae3d36c7861862784b1e6e53a6871
Mario Ponce <mario@mpondev.com>
2023-12-19 19:27:24 +0100

    finish Day 5 (2023)

--- 2023/Day 5/script.js
+++ 2023/Day 5/script.js
@@ -45,3 +45,65 @@ for (let seed of seeds) {
 }
 
 console.log(Math.min(...result)); // 346433842 (Test: 35)
+
+/*
+DAY 5 (II)
+What is the lowest location number that corresponds to any of the initial seed numbers?
+ */
+
+const seedRanges = seeds.reduce((result, _, index, array) => {
+  index % 2 === 0 && result.push(array.slice(index, index + 2));
+  return result;
+}, []);
+
+function convertRanges(seed, maps) {
+  let [newSeed, seedRange] = seed;
+  let minimum;
+
+  for (
+    let seedToCheck = newSeed;
+    seedToCheck <= newSeed + seedRange;
+    seedToCheck++
+  ) {
+    let newSeedToCheck = seedToCheck;
+
+    for (let map of maps) {
+      let mapConversion;
+
+      for (let line of map) {
+        let range = line[2];
+        let diff = line[0] - line[1];
+
+        if (
+          line[1] <= newSeedToCheck &&
+          newSeedToCheck <= line[1] + range &&
+          !mapConversion
+        ) {
+          mapConversion = newSeedToCheck + diff;
+          newSeedToCheck += diff;
+        }
+      }
+    }
+
+    if (!minimum) {
+      minimum = newSeedToCheck;
+    } else {
+      if (minimum > newSeedToCheck) {
+        minimum = newSeedToCheck;
+      }
+    }
+  }
+
+  return minimum;
+}
+
+const rangeResult = [];
+
+for (let seed of seedRanges) {
+  rangeResult.push(convertRanges(seed, maps));
+}
+
+console.log(Math.min(...rangeResult)); // 60294664 (Test: 46)
+
+// FIXME
+// Too slow solution
