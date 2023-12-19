92bf6063ca6687bae86cd19111e4da4fac45db81
Mario Ponce <mario@mpondev.com>
2023-12-19 19:40:01 +0100

    start Day 6 (2023)

---
+++ 2023/Day 6/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 6 (I)
+Determine the number of ways you could beat the record in each race. What do you get if you multiply these numbers together?
+ */
+
+import data from './input.js';
+
+// (Test: 288)
