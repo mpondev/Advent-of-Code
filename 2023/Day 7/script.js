0f1a1af4624199b703068fa67bf41577bce1ba7d
mpondev <mario@mpondev.com>
2023-12-25 12:48:48 +0100

    finish Day 7 (2023)

--- 2023/Day 7/script.js
+++ 2023/Day 7/script.js
@@ -8,7 +8,7 @@ Find the rank of every hand in your set. What are the total winnings?
 import data from './input.js';
 
 // Dictionary to convert cards to alphabet so we could order hands
-const cardConverter = {
+let cardConverter = {
   A: 'a',
   K: 'b',
   Q: 'c',
@@ -25,7 +25,7 @@ const cardConverter = {
 };
 
 // Prepare data
-const hands = data
+let hands = data
   .split('\n')
   .map(hand => hand.split(' '))
   .map(hand => [
@@ -109,3 +109,121 @@ getWinnings(hands); // 246163188 (Test: 6440)
 DAY 7 (I)
 Using the new joker rule, find the rank of every hand in your set. What are the new total winnings?
  */
+
+// Dictionary to convert cards to alphabet so we could order hands
+cardConverter = {
+  A: 'a',
+  K: 'b',
+  Q: 'c',
+  T: 'd',
+  9: 'e',
+  8: 'f',
+  7: 'g',
+  6: 'h',
+  5: 'i',
+  4: 'j',
+  3: 'k',
+  2: 'l',
+  J: 'm',
+};
+
+hands = data
+  .split('\n')
+  .map(hand => hand.split(' '))
+  .map(hand => [
+    hand[0].replace(/[AKQJT2-9]/gi, card => cardConverter[card]),
+    hand[1],
+  ]);
+
+function getWinningsJoker(hands) {
+  let handsOrdered = {};
+
+  for (let hand of hands) {
+    // Break down hand
+    let count = {};
+    for (let card of hand[0]) {
+      count[card] ? (count[card] += 1) : (count[card] = 1);
+    }
+
+    // Find most repeated card (different of Joker)
+    let sortCount = Object.entries(count).sort(([, a], [, b]) => b - a);
+    let max =
+      sortCount.length === 1
+        ? sortCount[0][0]
+        : sortCount[0][0] === 'm'
+        ? sortCount[1][0]
+        : sortCount[0][0];
+
+    if (count['m']) {
+      if (max === 'm') {
+        hand.push(hand[0]);
+      } else {
+        count[max] += count['m'];
+        delete count['m'];
+        hand.push(hand[0].replace(/m/g, max));
+      }
+    } else {
+      hand.push(hand[0]);
+    }
+
+    let uniqueCards = new Set(hand[2]);
+
+    switch (uniqueCards.size) {
+      case 1:
+        handsOrdered[1]
+          ? handsOrdered[1].push(hand)
+          : (handsOrdered[1] = [hand]);
+        break;
+      case 2:
+        if (Object.values(count).includes(4)) {
+          handsOrdered[2]
+            ? handsOrdered[2].push(hand)
+            : (handsOrdered[2] = [hand]);
+          break;
+        } else {
+          handsOrdered[3]
+            ? handsOrdered[3].push(hand)
+            : (handsOrdered[3] = [hand]);
+          break;
+        }
+      case 3:
+        if (!Object.values(count).includes(2)) {
+          handsOrdered[4]
+            ? handsOrdered[4].push(hand)
+            : (handsOrdered[4] = [hand]);
+          break;
+        } else {
+          handsOrdered[5]
+            ? handsOrdered[5].push(hand)
+            : (handsOrdered[5] = [hand]);
+          break;
+        }
+      case 4:
+        handsOrdered[6]
+          ? handsOrdered[6].push(hand)
+          : (handsOrdered[6] = [hand]);
+        break;
+      default:
+        handsOrdered[7]
+          ? handsOrdered[7].push(hand)
+          : (handsOrdered[7] = [hand]);
+        break;
+    }
+  }
+
+  for (const prop in handsOrdered) {
+    handsOrdered[prop].sort();
+  }
+
+  const handsOrderedArr = Object.values(handsOrdered).flat().reverse();
+
+  let total = 0;
+
+  for (let i = 0; i < handsOrderedArr.length; i++) {
+    total += handsOrderedArr[i][1] * (i + 1);
+  }
+
+  console.log(total);
+}
+
+getWinningsJoker(hands); // 245794069 (Test: 5905)
