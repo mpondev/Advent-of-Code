802e56566e06020c1943f6575b0b6182c9e46612
Mario Ponce <mario@mpondev.com>
2023-12-27 21:48:39 +0100

    start Day 8 part Two (2023)

--- 2023/Day 8/script.js
+++ 2023/Day 8/script.js
@@ -7,4 +7,43 @@ Find the rank of every hand in your set. What are the total winnings?
 
 import data from './input.js';
 
-// (Test: 2, 6)
+// Prepare data
+let [instructions, maps] = data.split('\n\n');
+
+let reg = /(\= \()|,|\)/g;
+maps = maps.replace(reg, '').split('\n');
+
+// Regular expressions to find first/middle/last element
+const reg1 = /^[A-Z0-9]{3}/;
+const reg2 = /(?<= )[A-Z0-9]{3}(?= )/;
+const reg3 = /[A-Z0-9]{3}$/;
+
+function findElement(element) {
+  for (let line of maps) {
+    if (line.match(reg1)[0] === element) {
+      return line;
+    }
+  }
+}
+
+function stepsToZ() {
+  let steps = 0;
+  let element = 'AAA';
+
+  while (element !== 'ZZZ') {
+    element =
+      instructions[steps % instructions.length] === 'L'
+        ? findElement(element).match(reg2)[0]
+        : findElement(element).match(reg3)[0];
+    steps++;
+  }
+
+  console.log(steps);
+}
+
+stepsToZ(); // 13939 (Test: 2, 6)
+
+/*
+DAY 8 (II)
+How many steps does it take before you're only on nodes that end with Z?
+ */
