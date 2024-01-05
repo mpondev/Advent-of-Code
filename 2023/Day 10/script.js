5687a867e7a9fca6af10f7efa2ed7c6f66529c75
mpondev <mario@mpondev.com>
2024-01-05 13:17:46 +0100

    start Day 10 (2023)

---
+++ 2023/Day 10/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 10 (I)
+How many steps along the loop does it take to get from the starting position to the point farthest from the starting position?
+ */
+
+import data from './input.js';
+
+// (Test: 4)
