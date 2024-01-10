d231d38504ec941fa266fb34477c1e0c0ef29590
Mario Ponce <mario@mpondev.com>
2024-01-10 19:18:23 +0100

    start Day 11 (2023)

---
+++ 2023/Day 11/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 11 (I)
+Expand the universe, then find the length of the shortest path between every pair of galaxies. What is the sum of these lengths?
+ */
+
+import data from './inputTest.js';
+
+// (Test: 374)
