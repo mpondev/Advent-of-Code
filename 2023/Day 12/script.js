3e5691fb524e93c5e0092202ec5535ec23aef716
mpondev <mario@mpondev.com>
2024-01-12 13:11:16 +0100

    start Day 12 (2023)

---
+++ 2023/Day 12/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 12 (I)
+For each row, count all of the different arrangements of operational and broken springs that meet the given criteria. What is the sum of those counts?
+ */
+
+import data from './input.js';
+
+// (Test: 21)
