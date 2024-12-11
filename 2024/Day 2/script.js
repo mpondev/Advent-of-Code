30d0d3c8b4293ada72c94042786beeecf1ce6273
Mario Ponce <mario@mpondev.com>
2024-12-11 07:14:20 +0100

    Finisg Day 2 part Pne (2024)

--- 2024/Day 2/script.js
+++ 2024/Day 2/script.js
@@ -7,4 +7,44 @@ Analyze the unusual data from the engineers. How many reports are safe?
 
 import data from './input.js';
 
-// (Test: 2)
+const reports = data.split('\n').map(report => report.split(' ').map(Number));
+
+function safeReports() {
+  let safeReports = 0;
+
+  reports.forEach(report => {
+    let safeLevels = 0;
+
+    for (let i = 1; i < report.length; i++) {
+      if (report[i] >= report[i - 1] + 1 && report[i] <= report[i - 1] + 3) {
+        safeLevels++;
+      } else {
+        break;
+      }
+    }
+    if (safeLevels === report.length - 1) {
+      safeReports++;
+    }
+  });
+
+  reports.forEach(report => {
+    let safeLevels = 0;
+
+    for (let i = 1; i < report.length; i++) {
+      if (report[i] <= report[i - 1] - 1 && report[i] >= report[i - 1] - 3) {
+        safeLevels++;
+      } else {
+        break;
+      }
+    }
+    if (safeLevels === report.length - 1) {
+      safeReports++;
+    }
+  });
+
+  return safeReports;
+}
+
+console.log(safeReports());
+
+// 299 (Test: 2)
