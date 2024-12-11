a202c888302d9fd36ef3ffb969afbd61ceaad8cf
mpondev <mario@mpondev.com>
2024-12-11 10:53:37 +0100

    start Day 2 part Two (2024)

--- 2024/Day 2/script.js
+++ 2024/Day 2/script.js
@@ -7,4 +7,44 @@ Analyze the unusual data from the engineers. How many reports are safe?
 
 import data from './input.js';
 
-// (Test: 2)
+const reports = data.split('\n').map(report => report.split(' ').map(Number));
+
+// Function to check the safety criteria
+function isSafeReport(report, comparator) {
+  for (let i = 1; i < report.length; i++) {
+    if (!comparator(report[i], report[i - 1])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function safeReports() {
+  let safeReportsCount = 0;
+
+  // Comparator functions
+  const isSafeIncrease = (current, previous) =>
+    current >= previous + 1 && current <= previous + 3;
+  const isSafeDecrease = (current, previous) =>
+    current <= previous - 1 && current >= previous - 3;
+
+  reports.forEach(report => {
+    if (
+      isSafeReport(report, isSafeIncrease) ||
+      isSafeReport(report, isSafeDecrease)
+    ) {
+      safeReportsCount++;
+    }
+  });
+
+  return safeReportsCount;
+}
+
+console.log(safeReports()); // 299 (Test: 2)
+
+/*
+DAY 2 (II)
+Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+ */
+
+// (Test: 4)
