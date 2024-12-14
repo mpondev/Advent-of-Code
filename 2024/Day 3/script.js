b2408a2227bc48579a961f2cc29e680d7dc2807f
Mario Ponce <mario@mpondev.com>
2024-12-14 19:55:02 +0100

    start Day 3 part Two (2024)

--- 2024/Day 3/script.js
+++ 2024/Day 3/script.js
@@ -2,7 +2,36 @@
 
 /*
 DAY 3 (I)
-Analyze the unusual data from the engineers. How many reports are safe?
+Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
  */
 
-// (Test: 161)
+// Fetch data. Template literal doesn't work here because of special characters.
+async function fetchData() {
+  try {
+    const response = await fetch('./input.txt');
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    const data = await response.text();
+
+    return data;
+  } catch (error) {
+    console.error('There has been a problem fetching data:', error);
+  }
+}
+
+const data = await fetchData();
+
+let mul = 0;
+for (let part of data.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)) {
+  mul += part[1] * part[2];
+}
+
+console.log(mul); // 187825547 (Test: 161)
+
+/*
+DAY 3 (II)
+Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
+ */
+
+// (Test: 48)
