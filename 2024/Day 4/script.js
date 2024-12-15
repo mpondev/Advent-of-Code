14e278df1a2be4d6828449ac175955c89ca912a7
Mario Ponce <mario@mpondev.com>
2024-12-15 11:22:14 +0100

    start Day 4 (2024)

---
+++ 2024/Day 4/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/*
+DAY 4 (I)
+Take a look at the little Elf's word search. How many times does XMAS appear?
+ */
+
+import data from './inputTest.js';
+
+console.log(data); //  (Test: 18)
